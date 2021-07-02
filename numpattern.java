d950312c25016ce527d10c9551ecadbbe4eb5482
yamunakandukuri <86612118+yamunakandukuri@users.noreply.github.com>
2021-07-02 20:44:50 +0530

    Create numpattern.java

---
+++ numpattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class numpattern
+{
+    public static void  main (String[] args) {
+       {
+           for(int i=1;i<=6;i++)
+           {
+               for(int j=1;j<=i;j++)
+               System.out.print(j);
+               System.out.println();
+           }
+       }
+    }
+}
